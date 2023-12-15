0896988e392c2c3c6a0fa03173fac29b2ca9b918
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-15 17:37:44 +0700

    1: init install go

---
+++ index.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// teminal go run index.go
+// sudo snap install go --classic
+
+// sudo snap install sqlc
+
+// insall go vscode
+
+// docker
