a0036fee53a2ec8cce1dbabffee74e1722ffb505
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-25 13:46:21 +0700

    33. validate payload request grpc

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -6,12 +6,20 @@ import (
 	db "github.com/TTKirito/go/db/sqlc"
 	"github.com/TTKirito/go/pb"
 	"github.com/TTKirito/go/util"
+	"github.com/TTKirito/go/val"
 	"github.com/lib/pq"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	violations := validateCreateUserRequest(req)
+
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	hashedPassword, err := util.HashedPassword(req.GetPassword())
 	if err != nil {
 		return nil, status.Errorf(codes.Unimplemented, "method CreateUser not implemented")
@@ -42,4 +50,24 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	}
 
 	return rsp, nil
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	if err := val.ValidateFullName(req.GetFullName()); err != nil {
+		violations = append(violations, fieldViolation("full_name", err))
+	}
+
+	if err := val.ValidateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, fieldViolation("email", err))
+	}
+
+	return violations
 }
